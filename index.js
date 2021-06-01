41fd48235d21e73ddb9b93bf5b8fcc861e9bc647
Abir Shandilya <82814195+abirshandilya07@users.noreply.github.com>
2021-06-01 18:36:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let a;
+let date;
+let time;  
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(() => {
+    a = new Date();
+    let hour = a.getHours();
+    if (hour > 12){
+        oof = "PM";
+        hour -= 12;
+    }
+    else if (hour<12){
+        oof = "AM";
+    }
+    else{
+        oof = ""
+    }
+    time = hour + ":" + a.getMinutes() + ":" + a.getSeconds() + " " +oof;
+    date = a.toLocaleDateString(undefined, options);
+    document.getElementById("time").innerHTML = time;
+    document.getElementById('date').innerHTML = date;
+}, 1000);
